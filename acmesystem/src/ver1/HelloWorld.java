725f43d2e8443ac7f82bfd1072bfdd787ac01712
Reginald Berry <rgberry@valdosta.edu>
2026-08-31 22:04:39 -0400

    A message.

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Dave is working locally on his branch");
 	}
 
 }
